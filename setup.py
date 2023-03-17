74e181d6f07d8d4b1eb59b23806c4a4e54eb3dd6
Ujjwal Chowdhury <90385123+U77w41@users.noreply.github.com>
2023-03-17 13:45:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
 long_description = (HERE / 'README.md').read_text(encoding='utf-8')
-__version__ = '0.0.8'
+__version__ = '0.1.1'
 __maintainer__ = 'Ujjwal Chowdhury'
 
 
